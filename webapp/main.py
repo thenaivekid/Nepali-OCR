7af6d6cbd74b51f84570352917f9adfc76b2790b
thenaivekid <neupane.ashok.9696@gmail.com>
2023-08-08 22:46:01 +0545

    frontend working perfectly after i removed image preview feature

--- webapp/main.py
+++ webapp/main.py
@@ -21,16 +21,18 @@ async def root():
 @app.post("/ocr")
 async def ocr(file: UploadFile = File(...)):
     contents = await file.read()
-    file_path = os.path.join("images", file.filename)
-    print(file_path)
-    with open(file_path, "wb") as f:
-        f.write(contents)
-    print(file.filename, "received")
-    nparr = np.fromstring(contents, np.uint8)
-    try:
+    # file_path = os.path.join("/images", file.filename)
+    file_path = file.filename
+    if file_path:
+        # print(file_path)
+        # with open(file_path, "wb") as f:
+        #     f.write(contents)
+        print(file.filename, "received")
+        nparr = np.fromstring(contents, np.uint8)
         image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    except cv2.error as e:
-        print("Error decoding image:", e)
-        return {"text": "", "image_url": file_path}
-    text = str(pipeline(image))
-    return {"text": text, "image_url": file_path}
+        
+        text = str(pipeline(image))
+        # return {"text": text, "image_url": str(file_path)}
+        return {"text": text}
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
